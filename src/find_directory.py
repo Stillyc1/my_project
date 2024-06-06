9afbc204c3be7d795e0aff4c81d509546d493b73
Alexander_Baranov <Sti_ll@mail.ru>
2024-06-06 11:22:09 +0300

    коммит инициализации

---
+++ src/find_directory.py
@@ -0,0 +1,27 @@
+import os
+""" Импортируем библиотеку os """
+
+
+def find_to_directory(folder_path: str = os.getcwd(), recursive_counting: str | int = None) -> dict:
+    """
+    Функция принимает путь до директории и возвращает кол-во файлов и папок в директории
+    """
+    directory_count_files = {"files": 0, "folders": 0}
+
+    if os.path.isdir(folder_path) is True:
+        find_dir = os.scandir(folder_path)
+
+        for file in find_dir:
+            if os.path.isfile(file) is True:
+                directory_count_files["files"] += 1
+            elif os.path.isdir(file) is True:
+                directory_count_files["folders"] += 1
+
+    if recursive_counting is not None:
+        for root, dirs, files in os.walk(folder_path):
+            for name in files:
+                print(os.path.join(root, name))
+            for name in dirs:
+                print(os.path.join(root, name))
+
+    return directory_count_files
---
+++ src/masks.py
@@ -0,0 +1,27 @@
+def get_mask_card_number(number_card: int | str) -> str:
+    """
+    Функция принимает номер карты и возвращает
+    номер карты с маскировкой 6ти цифр (с 7 - 12)
+
+    p.s. вход преобразуем в строку, заменяем цифры с 7-12,
+    по индексу делим номер карты на 4 блока,
+    возвращаем, разделяем пробелом
+    """
+
+    number_card_string = str(number_card)
+    mask_card = number_card_string.replace(number_card_string[6:12], "******")
+    number_card_divide = mask_card[:4], mask_card[4:8], mask_card[8:12], mask_card[12:]
+    mask_number_card = " ".join(number_card_divide)
+
+    return mask_number_card
+
+
+def get_mask_account(account_number: int | str) -> str:
+    """
+    Функция принимает номер счёта и возвращает маскировку счета
+    в формате: "** 4цифры счета"
+    """
+    
+    account_number_string = str(account_number)
+
+    return f"**{account_number_string[-4:]}"
