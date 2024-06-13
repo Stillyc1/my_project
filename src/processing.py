6e3a018ffaec15e0536d9fe5c8580e5d472dbebd
Alexander_Baranov <Sti_ll@mail.ru>
2024-06-13 16:40:46 +0300

    correct processing

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
 from typing import Any
 
 
-def filter_by_state(info_users: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(info_users: list[dict], state: str = "EXECUTED") -> list[dict]:
     sort_info_users = []
     for info in info_users:
-        if info['state'] == state:
+        if info["state"] == state:
             sort_info_users.append(info)
 
     return sort_info_users
@@ -12,6 +12,6 @@ def filter_by_state(info_users: list[dict], state: str = 'EXECUTED') -> list[dic
 
 def sort_by_date(info_dicts: list[dict], sorting_parameter: Any = None) -> list[dict]:
     if sorting_parameter is not None:
-        return sorted(info_dicts, key=lambda date: date['date'])
+        return sorted(info_dicts, key=lambda date: date["date"])
     else:
-        return sorted(info_dicts, key=lambda date: date['date'], reverse=True)
+        return sorted(info_dicts, key=lambda date: date["date"], reverse=True)
