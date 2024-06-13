c871683fc933f8c548d8aa7872a371bedb96b588
Alexander_Baranov <Sti_ll@mail.ru>
2024-06-13 12:43:44 +0300

    add func filter_by_state in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,3 @@
-
-
 def filter_by_state(info_users: list[dict], state: str = 'EXECUTED') -> list[dict]:
     sort_info_users = []
     for info in info_users:
@@ -7,6 +5,3 @@ def filter_by_state(info_users: list[dict], state: str = 'EXECUTED') -> list[dic
             sort_info_users.append(info)
 
     return sort_info_users
-
-
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
