9b14ecf33ff844f10b1e03eaac9344fbc162ccfa
Alexander_Baranov <Sti_ll@mail.ru>
2024-06-17 12:25:06 +0300

    add modul tests in tests

---
+++ tests/test_masks.py
@@ -0,0 +1,9 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def test_get_mask_account(num_account):
+    assert get_mask_account(num_account) == '**7890'
+
+
+def test_get_mask_card_number(number_card):
+    assert get_mask_card_number(number_card) == '1234 56** **** 3456'
