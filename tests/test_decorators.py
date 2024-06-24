2e04119db0b520b5c651f09de91131ed79caaf15
Alexander_Baranov <Sti_ll@mail.ru>
2024-06-24 22:55:47 +0300

    add tests in test_decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,4 +6,14 @@ def test_log():
     def my_function(x, y):
         return x + y
 
-    assert my_function(5, 3) == 8
+    assert my_function(1, 2) == 3
+
+
+def test_log_if_no_arg(capsys):
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert "3\nmy_function ok.\n" in captured.out
