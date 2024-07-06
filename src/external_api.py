72cbce3b7a2426bb9fb068261fdf667c4f79ae09
Stillyc1 <Sti_ll@mail.ru>
2024-07-06 19:30:55 +0300

    Update external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -9,20 +9,19 @@ API_KEY = os.getenv("API_KEY")
 URL_CONVERT = os.getenv("URL_CONVERT")
 
 
-def converting_amount_transactions(transaction: list[dict]) -> float:
+def converting_amount_transactions(transaction: dict) -> float:
     """
     Функция принимает транзакцию вида dict и возвращает сумму транзакции,
     в случае валюты EUR или USD конвертирует сумму в RUB по актуальному курсу
     """
-    for trans in transaction:
-        if trans["operationAmount"]["currency"]["code"] == "RUB":
-            return round(float(trans["operationAmount"]["amount"]), 2)
+        if transaction["operationAmount"]["currency"]["code"] == "RUB":
+            return round(float(transaction["operationAmount"]["amount"]), 2)
 
         else:
             url = URL_CONVERT
             payload = {
-                "amount": trans["operationAmount"]["amount"],
-                "from": trans["operationAmount"]["currency"]["code"],
+                "amount": transaction["operationAmount"]["amount"],
+                "from": transaction["operationAmount"]["currency"]["code"],
                 "to": "RUB",
             }
 
